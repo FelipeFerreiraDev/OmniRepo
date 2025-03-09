eb781eda4d0a6b50df8f9150d4a73261bc29078c
felipeferreiradev <felipeferreira.sp.dev@gmail.com>
2025-03-09 14:23:15 -0300

    feat: first project .net

---
+++ Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Category
+{
+    public class CategoryTest
+    {
+        [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Instantiate()
+        {
+            // Arrange
+            var validData = new
+            {
+                Name = "category name",
+                Description = "category description"
+            };
+
+            var datetimeBefore = DateTime.Now;
+
+            // Act
+            var category = new DomainEntity.Category(validData.Name, validData.Description);
+            
+            var datetimeAfter = DateTime.Now;
+
+            // Assert
+            Assert.NotNull(category);
+            Assert.Equal(validData.Name, category.Name);
+            Assert.Equal(validData.Description, category.Description);
+            Assert.NotEqual(default(Guid), category.Id);
+            Assert.NotEqual(default(DateTime), category.CreatedAt);
+            Assert.True(category.CreatedAt > datetimeBefore);
+            Assert.True(category.CreatedAt < datetimeAfter);
+            Assert.True(category.IsActive);
+        }
+    }
+}
